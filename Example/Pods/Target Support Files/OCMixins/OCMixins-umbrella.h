e0a1131d96616dd614755a7f72d9fd30fbf537c6
Henrique Morbin <morbin_@hotmail.com>
2016-12-18 19:48:31 -0200

    Adding pods to gitignore

--- Example/Pods/Target Support Files/OCMixins/OCMixins-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double OCMixinsVersionNumber;
-FOUNDATION_EXPORT const unsigned char OCMixinsVersionString[];
-
--- Example/Pods/Target Support Files/Pods-OCMixins_Tests/Pods-OCMixins_Tests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_OCMixins_TestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_OCMixins_TestsVersionString[];
-
