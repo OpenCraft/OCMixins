93a8be74d3cb8d471d7d253bd741ef7c6ebc1f44
Cristian Madrid <cristianmadridd@gmail.com>
2017-03-08 19:52:18 -0300

    Add navigationClosable and NavigationGradient

---
+++ OCMixins/Classes/NavigationBarGradient.swift
@@ -0,0 +1,86 @@
+//
+//  NavigationBarGradient.swift
+//  VidaPrevidencia
+//
+//  Created by Cristian Madrid on 20/01/17.
+//  Copyright © 2017 Porto Seguro. All rights reserved.
+//
+
+import UIKit
+public enum GradientOrientation {
+    case horizontal, vertical
+    
+    var startPoint: CGPoint {
+        switch self {
+        case .horizontal:
+            return CGPoint(x: 0.0, y: 0.5)
+        case .vertical:
+            return CGPoint(x: 0.5, y: 0)
+        }
+    }
+    
+    var endPoint: CGPoint {
+        switch self {
+        case .horizontal:
+            return CGPoint(x: 1.0, y: 0.5)
+        case .vertical:
+            return CGPoint(x: 0.5, y: 1)
+        }
+    }
+}
+
+protocol NavigationBarGradient {
+    func setupNavigationBarGradient(orientation: GradientOrientation, colors: [UIColor])
+}
+
+extension NavigationBarGradient where Self: UIViewController {
+    
+    func setupNavigationBarGradient(orientation: GradientOrientation, colors: [UIColor]) {
+        guard let navigationController = navigationController else {
+            return
+        }
+        
+        setupNavigationBarTransparent()
+        
+        let navigationBar = navigationController.navigationBar
+        let layer = getGradientLayer(orientation: orientation, colors: colors.flatMap { $0.cgColor })
+        
+        let width = UIApplication.shared.statusBarFrame.width
+        let height = UIApplication.shared.statusBarFrame.height + navigationController.navigationBar.frame.height
+        
+        let behindView = UIView(frame: CGRect(x: 0, y: 0, width: width, height: height))
+        behindView.layer.insertSublayer(layer, at: 0)
+        
+        navigationController.view.insertSubview(behindView, belowSubview: navigationBar)
+    }
+    
+    final fileprivate func setupNavigationBarTransparent() {
+        guard let navigationController = navigationController else {
+            return
+        }
+        
+        navigationController.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController.navigationBar.shadowImage = UIImage()
+        navigationController.navigationBar.isTranslucent = true
+    }
+    
+    final fileprivate func getGradientLayer(orientation: GradientOrientation, colors: [CGColor]) -> CAGradientLayer {
+        let layer = CAGradientLayer()
+        
+        guard let navigationController = navigationController else {
+            return layer
+        }
+        
+        layer.colors = colors
+        layer.startPoint = orientation.startPoint
+        layer.endPoint = orientation.endPoint
+        
+        let navBarFrame = navigationController.navigationBar.bounds
+        let statusFrame = UIApplication.shared.statusBarFrame
+        layer.frame = CGRect(x: navBarFrame.minX,
+                             y: 0,
+                             width: navBarFrame.width,
+                             height: navBarFrame.height + statusFrame.height + 1)
+        return layer
+    }
+}
